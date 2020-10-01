4e2f69a38dcebf3bcf63c55bdda85a6667f545b0
Ooi Wei Tsang <weitsang@yahoo.com>
2020-10-01 12:18:38 +0800

    first commit

---
+++ mastermind.c
@@ -0,0 +1,15 @@
+/**
+ * CS1010 Semester 1 AY20/21
+ * Assignment 4: Mastermind
+ *
+ * TODO: Write documentation
+ *
+ * @file: mastermind.c
+ * @author: XXX (Group YYY)
+ */
+#include "cs1010.h"
+#include <stdbool.h>
+
+int main()
+{
+}
---
+++ selectionsort.c
@@ -0,0 +1,13 @@
+/**
+ * CS1010 Semester 1 AY18/19
+ * Assignment 4: SelectionSort
+ *
+ * TODO: Write documentation
+ *
+ * @file: selectionsort.c
+ * @author: XXX (Group YYY)
+ */
+
+int main() 
+{
+}
